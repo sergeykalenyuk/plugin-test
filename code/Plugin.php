dd032be0ecde5e7bc72d07413a5aa8176187187f
Colin Mollenhour <colin@mollenhour.com>
2015-02-05 20:47:39 -0500

    Progress on OAuth integration.

--- code/Plugin.php
+++ code/Plugin.php
@@ -32,6 +32,8 @@ class Test_Test_Plugin extends Plugin_Abstract
     }
 
     /**
+     * Respond to order:created events.
+     *
      * @param array $data
      */
     public function respondOrderCreated($data)
@@ -39,7 +41,13 @@ class Test_Test_Plugin extends Plugin_Abstract
         $this->log("Order # {$data['unique_id']} was created.");
     }
 
+    /*
+     * Webhook Methods
+     */
+
     /**
+     * Verify webhook request authenticity.
+     *
      * @param array $query
      * @param array $headers
      * @param string $data
@@ -53,6 +61,8 @@ class Test_Test_Plugin extends Plugin_Abstract
     }
 
     /**
+     * If authenticity was verified, handle the webhook data (does not block response to webhook request).
+     *
      * @param array $query
      * @param array $headers
      * @param string $data
@@ -61,6 +71,83 @@ class Test_Test_Plugin extends Plugin_Abstract
     public function handleWebhook($query, $headers, $data)
     {
         $this->log("Received webhook: (".http_build_query($query)."):\n$data");
+    }
+
+    /*
+     * OAuth Methods
+     */
+
+    /**
+     * Validate that all necessary config fields are filled out.
+     *
+     * @throws Exception
+     */
+    public function oauthValidateConfig()
+    {
+        if ( ! preg_match('/\w+/', $this->getConfig('whoami'))) {
+            throw new Exception('Who Am I must contain only alphanumeric characters or underscores.');
+        }
+        if ( ! $this->getConfig('oauth_key')) {
+            throw new Exception('OAuth API Key is not configured.');
+        }
+        if ( ! $this->getConfig('oauth_secret')) {
+            throw new Exception('OAuth API Secret is not configured.');
+        }
+    }
+
+    /**
+     * Get the URL that the user will visit to setup the OAuth connection
+     *
+     * @return string
+     */
+    public function oauthGetConnectButton()
+    {
+        $apiKey = urlencode($this->getConfig('oauth_api_key'));
+        $clientId = urlencode($this->getConfig('whoami'));
+        $scopes = urlencode('foo,bar,baz');
+        $redirect = urlencode($this->oauthGetRedirectUrl(['secret' => 'foo']));
+        $url = "http://www.example.com/oauth_test.php?action=authorize&api_key=$apiKey&client=$clientId&scopes=$scopes&redirect_uri=$redirect";
+        return '<a href="'.$url.'">Connect To OAuth Test</a>';
+    }
+
+    /**
+     * @param $request
+     */
+    public function oauthHandleRedirect($request)
+    {
+        $this->oauthSetTokenData($request['secret']);
+    }
+
+    /**
+     * @return array Key-value pairs of test results
+     * @throws Exception
+     */
+    public function oauthTest()
+    {
+        $response = ''; // TODO $this->oauthClient()->request('test'); or similar
+        throw new Exception('API not yet working..');
+        return [
+            'TODO' => 'Return data from live API to prove working operation.'
+        ];
+    }
+
+    /*
+     * Make these methods abstract
+     */
+
+    public function oauthGetRedirectUrl($params = array())
+    {
+        return ''; // TODO - web server controller url
+    }
+
+    public function oauthSetTokenData($secret)
+    {
+
+    }
+
+    public function oauthGetTokenData()
+    {
+
     }
 
 }
